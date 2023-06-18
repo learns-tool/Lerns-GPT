6dc99138bdefe58d89f3a006976ca41e6e962300
learns-tool <116647459+learns-tool@users.noreply.github.com>
2023-06-18 17:57:32 +0800

    Update layout.tsx
    修改网页标题和SEO信息

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "智能AI获客系统-写作机器人",
+  description: "Your intelligent AI writing robot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -17,7 +17,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "智能AI获客系统-写作机器人",
     statusBarStyle: "default",
   },
 };
